96003b0539ed263ddd73c479640bd1261d4b0edd
Thomas Ballinger <thomasballinger@gmail.com>
2012-10-10 23:01:35 -0400

    separate torrent file added

--- main.py
+++ main.py
@@ -1,45 +1,12 @@
 import bencode
 import urllib
-import sha
 import time
 import socket
-import datetime
 
 import bitstring
 
 import msg
-
-class Torrent(object):
-    """Torrent file data"""
-    class ParsingException(Exception): pass
-    def __init__(self, filename):
-        self.filename = filename
-        self.initialize_from_torrent_file()
-    def __str__(self):
-        return '<Torrent Object at '+str(id(self))+'; contents: '+self.name+'>'
-    def __repr__(self): return 'Torrent("'+self.filename+'")'
-
-    def initialize_from_torrent_file(self):
-        torrent_dict = bencode.bdecode(open(self.filename).read())
-        self.creation_date = datetime.datetime.fromtimestamp(torrent_dict['creation date'])
-        self.announce_url = torrent_dict['announce']
-        self.created_by = torrent_dict.get('created by', None)
-        self.encoding = torrent_dict.get('encoding', None)
-        info_dict = torrent_dict['info']
-        self.info_hash = sha.new(bencode.bencode(info_dict)).digest()
-        self.piece_length = info_dict['piece length']
-        self.piece_hashes = [info_dict['pieces'][i:i+20] for i in range(0, len(info_dict['pieces']), 20)]
-        self.private = bool(info_dict.get('private', 0))
-        if 'files' in info_dict:
-            self.mode = 'multi-file'
-            self.length = sum([f['length'] for f in info_dict['files']])
-            self.name = '; '.join([f['path'][0] for f in info_dict['files']])
-        else:
-            self.mode = 'single-file'
-            self.name = info_dict['name']
-            self.length = info_dict['length']
-        if self.length > len(self.piece_hashes) * self.piece_length:
-            raise Torrent.ParsingException('File size is greater than total size of all pieces')
+from torrent import Torrent
 
 class ActiveTorrent(Torrent):
     """Keeps track of what's been downloaded, stores data"""
@@ -108,7 +75,12 @@ class BittorrentClient(object):
         return peer
 
 class Peer(object):
-    """Represents a connection to a peer regarding a specific torrent"""
+    """Represents a connection to a peer regarding a specific torrent
+
+    >>> class FakeTorrent(): piece_hashes = ['1','2','3']
+    >>> p = Peer('fakeclient', FakeTorrent, ('123.123.123.123', 1234)); p
+
+    """
     def __init__(self, client, torrent, (ip, port)):
         self.ip = ip
         self.port = port
@@ -123,7 +95,8 @@ class Peer(object):
         self.handshook = False
         self.parsed_last_message = time.time()
         self.connect()
-
+    def __repr__(self):
+        return '<Peer {ip}:{port}>'.format(self.ip, self.port)
 
     def connect(self):
         """Establishes TCP connection to peer and sends handshake"""
@@ -192,9 +165,6 @@ class Peer(object):
 def main():
     client = BittorrentClient()
     t = ActiveTorrent('/Users/tomb/Downloads/How To Speed Up Your BitTorrent Downloads [mininova].torrent')
-    #t = ActiveTorrent('/Users/tomb/Downloads/Probity - THE ELECTRONiC CONNECTiON 28 [Trance-House-Progressive] [mininova].torrent')
-    #t = ActiveTorrent('/Users/tomb/Downloads/The best social Forex trading platform - follow the leaders and make a fortune [mininova].torrent')
-    #t = ActiveTorrent('/Users/tomb/Desktop/test.torrent')
     print repr(t)
     announce_data = client.announce(t)
     print announce_data
@@ -209,4 +179,5 @@ def test():
     doctest.testmod(optionflags=doctest.ELLIPSIS)
 
 if __name__ == '__main__':
-    main()
+    #main()
+    test()
---
+++ torrent.py
@@ -0,0 +1,49 @@
+"""Torrent file object
+
+>>> t = Torrent('./example.torrent'); t
+Torrent('./example.torrent')
+"""
+
+import datetime
+import sha
+import bencode
+
+class Torrent(object):
+    """Torrent file data"""
+    def __init__(self, filename):
+        self.filename = filename
+        self.initialize_from_torrent_file()
+    def __str__(self):
+        return '<Torrent Object at '+str(id(self))+'; contents: '+self.name+'>'
+    def __repr__(self): return 'Torrent(\''+self.filename+'\')'
+
+    class ParsingException(Exception): pass
+
+    def initialize_from_torrent_file(self):
+        torrent_dict = bencode.bdecode(open(self.filename).read())
+        self.creation_date = datetime.datetime.fromtimestamp(torrent_dict['creation date'])
+        self.announce_url = torrent_dict['announce']
+        self.created_by = torrent_dict.get('created by', None)
+        self.encoding = torrent_dict.get('encoding', None)
+        info_dict = torrent_dict['info']
+        self.info_hash = sha.new(bencode.bencode(info_dict)).digest()
+        self.piece_length = info_dict['piece length']
+        self.piece_hashes = [info_dict['pieces'][i:i+20] for i in range(0, len(info_dict['pieces']), 20)]
+        self.private = bool(info_dict.get('private', 0))
+        if 'files' in info_dict:
+            self.mode = 'multi-file'
+            self.length = sum([f['length'] for f in info_dict['files']])
+            self.name = '; '.join([f['path'][0] for f in info_dict['files']])
+        else:
+            self.mode = 'single-file'
+            self.name = info_dict['name']
+            self.length = info_dict['length']
+        if self.length > len(self.piece_hashes) * self.piece_length:
+            raise Torrent.ParsingException('File size is greater than total size of all pieces')
+
+def test():
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS)
+
+if __name__ == '__main__':
+    test()
