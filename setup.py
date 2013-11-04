9433321b646bb0867395b1b973371939af81a42d
Thomas Ballinger <thomasballinger@gmail.com>
2013-11-04 17:41:47 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bittorrent',
+      version='0.0.1',
+      description='bittorrent client',
+      url='https://github.com/thomasballinger/bittorrent',
+      author='Thomas Ballinger',
+      author_email='thomasballinger@gmail.com',
+      license='MIT',
+      packages=['bittorrent'],
+      zip_safe=False)
